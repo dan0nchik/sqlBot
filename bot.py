28279bd70511575937ca0cbd93fcf4260c267688
Daniel Khromov <danielkhromov@gmail.com>
2020-04-01 20:11:46 +0300

    сохраняет ники

--- bot.py
+++ bot.py
@@ -1,45 +1,33 @@
 # 1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk
-from telegram import ChatAction
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
-from functools import wraps
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 import sqlite3
 from sqlite3 import Error
+import os
 
 updater = Updater(token='1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk', use_context=True)
 dispatcher = updater.dispatcher
 
 
-# функция чтобы показать, что бот печатает
-def send_typing_action(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
-        return func(update, context, *args, **kwargs)
-
-    return command_func
-
-
 def start(update, context):
     user = update.message.from_user
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, {}! Я - SQL бот! Напиши /help, "
-                                                                    "чтобы узнать, что я могу".format(user.first_name))
-    print(user.username)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привет, <b>{user.first_name}!</b>"
+                                                                    f" Напиши /help чтобы узнать, что я могу",
+                             parse_mode=telegram.ParseMode.HTML)
 
 
 def _help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Вот мои команды: "
                                                                     "/sql - я выведу тебе самые популярные запросы"
                                                                     " языка SQL"
-                                                                    "")
+                                                                    "  /add_nick - добавлю твой никнейм в нашу базу "
+                                                                    "данных")
 
 
 def sql(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="*[CREATE TABLE]("
-                                                                    "https://www.bitdegree.org/learn/sql-commands-list#create-table)*",
+                                                                    "https://www.bitdegree.org/learn/sql-commands"
+                                                                    "-list#create-table)*",
                              parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 
@@ -55,14 +43,37 @@ def create_connection(file):
         print(e)
     finally:
         if connection:
-            connection.close()
+            cursor = connection.cursor()
+            cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users'; ''')
+            if cursor.fetchone()[0] == 1:
+                print('Table exists')
+            else:
+                cursor.execute("""CREATE TABLE users (name TEXT, surname TEXT, username TEXT PRIMARY KEY);""")
+                connection.commit()
+                connection.close()
+
+
+def addNicks(update, context):
+    user = update.message.from_user
+    connection = sqlite3.connect(file)
+    cursor = connection.cursor()
+    print(user.first_name)
+    print(user.last_name)
+    print(user.username)
+    cursor.execute("""INSERT INTO users (name, surname, username) VALUES 
+    ('{0}', '{1}', '{2}');""".format(user.first_name, user.last_name, user.username))
+    connection.commit()
+    connection.close()
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Добавлено! Больше эту команду можно не вызывать :)")
 
 
 if __name__ == '__main__':
-    create_connection(r"C:\Users\stupi\PycharmProjects\sqlBot\users.db")
+    file = os.getcwd() + r"\users.db"
+    create_connection(file)
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', _help))
     dispatcher.add_handler(CommandHandler('sql', sql))
+    dispatcher.add_handler(CommandHandler('add_nick', addNicks))
     # после этого хэндлера команды в dispatcher добавлять нельзя
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
     updater.start_polling()
