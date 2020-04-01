46c58e42916975fd7f5ee999b4a44de1dd5bfc5e
Daniel Khromov <danielkhromov@gmail.com>
2020-04-01 13:56:56 +0300

    обращается по имени + создает базу данных при запуске

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 # 1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk
 from telegram import ChatAction
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 from functools import wraps
 import telegram
+import sqlite3
+from sqlite3 import Error
 
 updater = Updater(token='1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk', use_context=True)
 dispatcher = updater.dispatcher
@@ -21,8 +24,10 @@ def send_typing_action(func):
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я - SQL бот! Напиши /help, "
-                                                                    "чтобы узнать, что я могу")
+    user = update.message.from_user
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, {}! Я - SQL бот! Напиши /help, "
+                                                                    "чтобы узнать, что я могу".format(user.first_name))
+    print(user.username)
 
 
 def _help(update, context):
@@ -42,10 +47,23 @@ def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Извини, я тебя не понял :(")
 
 
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('help', _help))
-dispatcher.add_handler(CommandHandler('sql', sql))
-# после этого хэндлера команды в dispatcher добавлять нельзя
-dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-updater.start_polling()
-updater.idle()
+def create_connection(file):
+    connection = None
+    try:
+        connection = sqlite3.connect(file)
+    except Error as e:
+        print(e)
+    finally:
+        if connection:
+            connection.close()
+
+
+if __name__ == '__main__':
+    create_connection(r"C:\Users\stupi\PycharmProjects\sqlBot\users.db")
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', _help))
+    dispatcher.add_handler(CommandHandler('sql', sql))
+    # после этого хэндлера команды в dispatcher добавлять нельзя
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    updater.start_polling()
+    updater.idle()
