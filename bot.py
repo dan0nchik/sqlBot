f7fb526c48f96f4ad0e9bc9711e28b86573107d9
Daniel Khromov <danielkhromov@gmail.com>
2020-04-03 17:41:25 +0300

    HTML parse (робит) + почистил код, но не рабоатет stop()

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# 1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 import telegram
 import sqlite3
@@ -12,26 +11,23 @@ dispatcher = updater.dispatcher
 
 def start(update, context):
     user = update.message.from_user
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привет, <b>{user.first_name}!</b>"
-                                                                    f" Напиши /help чтобы узнать, что я могу",
-                             parse_mode=telegram.ParseMode.HTML)
+    update.message.reply_text(f"Привет, <b>{user.first_name}!</b>"f" Напиши /help чтобы узнать, что я могу",
+                              parse_mode=telegram.ParseMode.HTML)
 
 
 def _help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Вот мои команды: "
-                                                                    "\n/sql - я выведу тебе самые популярные запросы "
-                                                                    "языка SQL:\n"
-                                                                    "/add_nick - добавлю твой никнейм в нашу базу "
-                                                                    "данных\n/get_db - отправлю "
-                                                                    "тебе базу данных никнеймов\n"
-                                                                    "/create_db - создам для тебя базу данных")
+    update.message.reply_text("Вот мои команды: ""\n/sql - я выведу тебе самые популярные запросы "
+                              "языка SQL:\n"
+                              "/add_nick - добавлю твой никнейм в нашу базу "
+                              "данных\n/get_db - отправлю "
+                              "тебе базу данных никнеймов\n"
+                              "/create_db - создам для тебя базу данных")
 
 
 def sql(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='<b><a href = "https://www.bitdegree.org/learn/sql-commands-list#andor">AND|OR</a></b>\n'
-                             '<b><a href = "https://www.bitdegree.org/learn/sql-commands-list#alter-table">ALTER TABLE</a></b>\n',
-                             parse_mode=telegram.ParseMode.HTML)
+    update.message.reply_text('<b><a href = "https://www.bitdegree.org/learn/sql-commands-list#andor">AND|OR</a></b>\n'
+                              '<b><a href = "https://www.bitdegree.org/learn/sql-commands-list#alter-table">ALTER TABLE</a></b>\n',
+                              parse_mode=telegram.ParseMode.HTML)
     # "\n*[ALTER TABLE](https://www.bitdegree.org/learn/sql-commands-list#alter-table)*"
     # "\n*[AS (alias)](https://www.bitdegree.org/learn/sql-commands-list#as-alias)*"
     # "\n*[BETWEEN](https://www.bitdegree.org/learn/sql-commands-list#between)*"
@@ -71,60 +67,53 @@ def sql(update, context):
 
 
 def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Извини, я тебя не понял :(")
+    update.message.reply_text("Извини, я тебя не понял :(")
 
 
 def sendBase(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Тссс, это секретная информация...")
+    update.message.reply_text("Тссс, это секретная информация...")
     context.bot.send_document(chat_id=update.effective_chat.id, document=open(r"{}\users.db".format(os.getcwd()), 'rb'))
 
 
 def startCreatingDB(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Ты всегда можешь отменить создание базы "
-                                                                    "командой /cancel. Готов? Пиши ок")
+    update.message.reply_text("Ты всегда можешь отменить создание базы командой /stop. Готов? Пиши ок")
     return 1
 
 
 def getDBName(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Ну что, создадим базу? А как она будет называться?")
+    update.message.reply_text("Ну что, создадим базу? А как она будет называться?")
     return 2
 
 
 def column(update, context):
     context.user_data['name'] = update.message.text
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"А как будет называться колонка? "
-                                                                    "(Она у нас будет одна, больше делать мне лень)")
+    update.message.reply_text(f"А как будет называться колонка? (Она у нас будет одна, больше делать мне лень)")
     return 3
 
 
 def columnType(update, context):
     replies = [['INTEGER', 'TEXT']]
     context.user_data['colName'] = update.message.text
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Неплохо, записал. "
-                                                                    f"Какой тип будет у колонки (INTEGER или TEXT)?",
-
-                             reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True))
+    update.message.reply_text(f"Неплохо, записал. \n" f"Какой тип будет у колонки (INTEGER или TEXT)?",
+                              reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True))
     return 4
 
 
 def createDBUserData(update, context):
     context.user_data['type'] = update.message.text
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Обрабатываю твою информацию...",
-                             reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text("Обрабатываю твою информацию...", reply_markup=ReplyKeyboardRemove())
     connection = sqlite3.connect(os.getcwd() + r"\{}.db".format(context.user_data['name']))
     cursor = connection.cursor()
     cursor.execute(f"""CREATE TABLE {context.user_data['name']}
                                  ({context.user_data['colName']} {context.user_data['type']});""")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Создал! Отправляю...")
+    update.message.reply_text("Создал! Отправляю...")
 
     context.bot.send_document(chat_id=update.effective_chat.id, document=
     open(os.getcwd() + r"\{}.db".format(context.user_data['name']), 'rb'))
-
     return ConversationHandler.END
 
 
-def cancel(update, context):
+def stop(update, context):
     update.message.reply_text('Пока, жалко базу не сделали :(')
     return ConversationHandler.END
 
@@ -140,7 +129,7 @@ def create_connection(file):
             cursor = connection.cursor()
             cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users'; ''')
             if cursor.fetchone()[0] == 1:
-                print('Table exists')
+                print("Table exists")
             else:
                 cursor.execute("""CREATE TABLE users (name TEXT, surname TEXT, username TEXT PRIMARY KEY);""")
                 connection.commit()
@@ -151,15 +140,11 @@ def addNicks(update, context):
     user = update.message.from_user
     connection = sqlite3.connect(file)
     cursor = connection.cursor()
-    print(user.first_name)
-    print(user.last_name)
-    print(user.username)
     cursor.execute("""INSERT INTO users (name, surname, username) VALUES 
     ('{0}', '{1}', '{2}');""".format(user.first_name, user.last_name, user.username))
     connection.commit()
     connection.close()
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Добавлено! Больше эту команду можно не вызывать :)")
+    update.message.reply_text("Добавлено! Больше эту команду можно не вызывать :)")
 
 
 if __name__ == '__main__':
@@ -170,21 +155,19 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler('sql', sql))
     dispatcher.add_handler(CommandHandler('add_nick', addNicks))
     dispatcher.add_handler(CommandHandler('get_db', sendBase))
-
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('create_db', startCreatingDB)],
         states={
             1: [MessageHandler(Filters.text, getDBName)],
 
-            2: [MessageHandler(Filters.text, column)],
+            2: [MessageHandler(Filters.text, column, pass_user_data=True)],
 
-            3: [MessageHandler(Filters.text, columnType)],
+            3: [MessageHandler(Filters.text, columnType, pass_user_data=True)],
 
-            4: [MessageHandler(Filters.text, createDBUserData)]
+            4: [MessageHandler(Filters.text, createDBUserData, pass_user_data=True)]
         },
 
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
+        fallbacks=[CommandHandler('stop', stop)])
     dispatcher.add_handler(conv_handler)
     # после этого хэндлера команды в dispatcher добавлять нельзя
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
