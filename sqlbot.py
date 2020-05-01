397bd56da2c8a39938eb618fe53cc92acd376798
Daniel Khromov <danielkhromov@gmail.com>
2020-05-01 17:25:13 +0000

    add repl.it file system support

--- sqlbot.py
+++ sqlbot.py
@@ -10,28 +10,33 @@ import keep_alive
 
 keep_alive.keep_alive()
 
-
-updater = Updater(token='1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk', use_context=True)
+updater = Updater(
+    token='1139407419:AAEgdKd38btTS0VIkUVA-Yld968KHc_D4yk', use_context=True)
 dispatcher = updater.dispatcher
 
 
 def start(update, context):
     user = update.message.from_user
-    update.message.reply_text(f"Бип Боп. Привет, <b>{user.first_name}!</b>"f" Напиши /help чтобы узнать, что я могу",
-                              parse_mode=telegram.ParseMode.HTML)
+    update.message.reply_text(
+        f"Бип Боп. Привет, <b>{user.first_name}!</b>"
+        f" Напиши /help чтобы узнать, что я могу",
+        parse_mode=telegram.ParseMode.HTML)
 
 
 def _help(update, context):
-    update.message.reply_text("Вот мои команды: ""\n/sql - я выведу тебе самые популярные запросы "
-                              "языка SQL:\n"
-                              "/create_db - создам для тебя базу\n"
-                              "/insert - заполню твою таблицу\n"
-                              "/add_column - добавлю колонку в таблицу\n")
+    update.message.reply_text(
+        "Вот мои команды: "
+        "\n/sql - я выведу тебе самые популярные запросы "
+        "языка SQL:\n"
+        "/create_db - создам для тебя базу\n"
+        "/insert - заполню твою таблицу\n"
+        "/add_column - добавлю колонку в таблицу\n")
 
 
 def askSQLCommand(update, context):
-    update.message.reply_text("Введи команду SQLLite которая тебе нужна, и я пришлю тебе ссылку с ней.\n"
-                              "Например, alter table")
+    update.message.reply_text(
+        "Введи команду SQLLite которая тебе нужна, и я пришлю тебе ссылку с ней.\n"
+        "Например, alter table")
     return 1
 
 
@@ -54,27 +59,34 @@ def startCreatingDB(update, context):
 
 
 def getDBName(update, context):
-    update.message.reply_text("Ну что, создадим базу? А как она будет называться?")
+    update.message.reply_text(
+        "Ну что, создадим базу? А как она будет называться?")
     return 2
 
 
 def column(update, context):
     context.user_data['name'] = update.message.text
-    update.message.reply_text(f"А как будет называться колонка? (Она у нас будет одна, больше делать мне лень)")
+    update.message.reply_text(
+        f"А как будет называться колонка? (Она у нас будет одна, больше делать мне лень)"
+    )
     return 3
 
 
 def columnType(update, context):
     replies = [['INTEGER', 'TEXT']]
     context.user_data['colName'] = update.message.text
-    update.message.reply_text(f"Неплохо, записал. \n" f"Какой тип будет у колонки (INTEGER или TEXT)?",
-                              reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True, resize_keyboard=True))
+    update.message.reply_text(
+        f"Неплохо, записал. \n"
+        f"Какой тип будет у колонки (INTEGER или TEXT)?",
+        reply_markup=ReplyKeyboardMarkup(
+            replies, one_time_keyboard=True, resize_keyboard=True))
     return 4
 
 
 def createDBUserData(update, context):
     context.user_data['type'] = update.message.text
-    update.message.reply_text("Обрабатываю твою информацию...", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(
+        "Обрабатываю твою информацию...", reply_markup=ReplyKeyboardRemove())
     user = update.message.from_user
     folder = user.username
     if folder is None:
@@ -83,14 +95,14 @@ def createDBUserData(update, context):
             folder = user.first_name
     if not os.path.exists(folder):
         try:
-            os.mkdir(os.getcwd() + r"\{}".format(folder))
+            os.mkdir(os.getcwd() + r"/{}".format(folder))
         except OSError as error:
             print(error)
 
-    print(f"Имя базы {context.user_data['name']}")
-    print(f"Имя колонки {context.user_data['colName']}")
-    print(f"Тип {context.user_data['type']}")
-    path = os.getcwd() + r"\{}".format(folder) + r"\{}.db".format(context.user_data['name'])
+    
+    path = os.getcwd() + r"/{}".format(folder) + r"/{}.db".format(
+        context.user_data['name'])
+
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
     cursor.execute(f"""CREATE TABLE {context.user_data['name']}
@@ -99,7 +111,8 @@ def createDBUserData(update, context):
     connection.commit()
     connection.close()
     update.message.reply_text("Создал! Отправляю...")
-    context.bot.send_document(chat_id=update.effective_chat.id, document=open(path, 'rb'))
+    context.bot.send_document(
+        chat_id=update.effective_chat.id, document=open(path, 'rb'))
     return ConversationHandler.END
 
 
@@ -112,12 +125,16 @@ def create_connection(file):
     finally:
         if connection:
             cursor = connection.cursor()
-            cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users'; ''')
+            cursor.execute(
+                ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users'; '''
+            )
             if cursor.fetchone()[0] == 1:
                 print("Таблица с никами существует")
             else:
                 print("Таблицы с никами пока нет")
-                cursor.execute("""CREATE TABLE users (name TEXT, surname TEXT, username TEXT PRIMARY KEY);""")
+                cursor.execute(
+                    """CREATE TABLE users (name TEXT, surname TEXT, username TEXT PRIMARY KEY);"""
+                )
 
 
 def startFillingTable(update, context):
@@ -127,36 +144,46 @@ def startFillingTable(update, context):
         folder = user.last_name
         if folder is None:
             folder = user.first_name
-    path = os.getcwd() + r"\{}".format(folder)
+    path = os.getcwd() + r"/{}".format(folder)
+    print(path)
     tables = [[os.listdir(path)]]
-    update.message.reply_text("Внимание! Если ты еще не создал таблицы - тебе придется остановить меня :("
-                              " В другом случае, какую таблицу ты хочешь выбрать?",
-                              reply_markup=ReplyKeyboardMarkup(tables[0], one_time_keyboard=True, resize_keyboard=True))
+    update.message.reply_text(
+        "Внимание! Если ты еще не создал таблицы - тебе придется остановить меня :("
+        " В другом случае, какую таблицу ты хочешь выбрать?",
+        reply_markup=ReplyKeyboardMarkup(
+            tables[0], one_time_keyboard=True, resize_keyboard=True))
     return 1
 
 
 def getTableNameToFill(update, context):
     context.user_data['table'] = update.message.text
-    update.message.reply_text("ОК, а какую колонку мы будем заполнять?", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(
+        "ОК, а какую колонку мы будем заполнять?",
+        reply_markup=ReplyKeyboardRemove())
     user = update.message.from_user
     folder = user.username
     if folder is None:
         folder = user.last_name
         if folder is None:
             folder = user.first_name
-    path = os.getcwd() + r"\{}".format(folder) + r"\{}".format(context.user_data['table'])
+    path = os.getcwd() + r"/{}".format(folder) + r"/{}".format(
+        context.user_data['table'])
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
-    cursor.execute("""SELECT * FROM {};"""
-                   .format(context.user_data['table'][:-3]))
-    names = [[list(map(lambda x: x[0], cursor.description))]]  # массив из колонок в таблице
+    cursor.execute("""SELECT * FROM {};""".format(
+        context.user_data['table'][:-3]))
+    names = [[list(map(lambda x: x[0],
+                       cursor.description))]]  # массив из колонок в таблице
     names[0][0].pop(0)  # удаляем id во избежание конфликтов
-    update.message.reply_text("Вот колонки", reply_markup=ReplyKeyboardMarkup(names[0], one_time_keyboard=True))
+    update.message.reply_text(
+        "Вот колонки",
+        reply_markup=ReplyKeyboardMarkup(names[0], one_time_keyboard=True))
     return 2
 
 
 def getColumnNameToFill(update, context):
-    update.message.reply_text("ОК, записал.", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(
+        "ОК, записал.", reply_markup=ReplyKeyboardRemove())
     context.user_data['column'] = update.message.text
     user = update.message.from_user
     folder = user.username
@@ -164,19 +191,22 @@ def getColumnNameToFill(update, context):
         folder = user.last_name
         if folder is None:
             folder = user.first_name
-    path = os.getcwd() + r"\{}".format(folder) + r"\{}".format(context.user_data['table'])
+    path = os.getcwd() + r"/{}".format(folder) + r"/{}".format(
+        context.user_data['table'])
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
-    cursor.execute(f"""PRAGMA TABLE_INFO ({context.user_data['table'][:-3]}) ;""")
+    cursor.execute(
+        f"""PRAGMA TABLE_INFO ({context.user_data['table'][:-3]}) ;""")
     rows = cursor.fetchall()
     for row in rows:
         if row[1] == context.user_data['column']:
             colType = row[2]
             break
     context.user_data['type'] = colType
-    update.message.reply_text(f"Внимание!!! У этой колонки тип {colType}. Это значит, что ты можешь вводить "
-                              f"значения "
-                              f"ТОЛЬКО этого типа. Иначе все полетит!")
+    update.message.reply_text(
+        f"Внимание!!! У этой колонки тип {colType}. Это значит, что ты можешь вводить "
+        f"значения "
+        f"ТОЛЬКО этого типа. Иначе все полетит!")
     return 3
 
 
@@ -189,66 +219,78 @@ def fillTableWithUserData(update, context):
         folder = user.last_name
         if folder is None:
             folder = user.first_name
-    path = os.getcwd() + r"\{}".format(folder) + r"\{}".format(context.user_data['table'])
+    path = os.getcwd() + r"/{}".format(folder) + r"/{}".format(
+        context.user_data['table'])
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
     if context.user_data['type'] == 'INTEGER':
         cursor.execute(
-            f"""INSERT INTO {context.user_data['table'][:-3]} ({context.user_data['column']}) VALUES({context.user_data['value']});""")
+            f"""INSERT INTO {context.user_data['table'][:-3]} ({context.user_data['column']}) VALUES({context.user_data['value']});"""
+        )
         connection.commit()
 
-    if context.user_data['type'] == 'TEXT':  # тут кавычки добавляются так как TEXT
+    if context.user_data[
+            'type'] == 'TEXT':  # тут кавычки добавляются так как TEXT
         cursor.execute(
-            f"""INSERT INTO {context.user_data['table'][:-3]} ({context.user_data['column']}) VALUES ('{context.user_data['value']}');""")
+            f"""INSERT INTO {context.user_data['table'][:-3]} ({context.user_data['column']}) VALUES ('{context.user_data['value']}');"""
+        )
         connection.commit()
-    context.bot.send_document(chat_id=update.effective_chat.id, document=open(path, 'rb'))
+    context.bot.send_document(
+        chat_id=update.effective_chat.id, document=open(path, 'rb'))
     update.message.reply_text("Готово!")
     return ConversationHandler.END
 
 
 def newColumnName(update, context):
     context.user_data['table'] = update.message.text
-    update.message.reply_text("А как будет называться колонка?", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(
+        "А как будет называться колонка?", reply_markup=ReplyKeyboardRemove())
     return 2
 
 
 def newColumnType(update, context):
     replies = [['INTEGER', 'TEXT']]
     context.user_data['colName'] = update.message.text
-    update.message.reply_text(f"Неплохо, записал. \n" f"Какой тип будет у колонки (INTEGER или TEXT)?",
-                              reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True))
+    update.message.reply_text(
+        f"Неплохо, записал. \n"
+        f"Какой тип будет у колонки (INTEGER или TEXT)?",
+        reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True))
     return 3
 
 
 def alterTable(update, context):
     context.user_data['type'] = update.message.text
-    update.message.reply_text("Обрабатываю твою информацию...", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text(
+        "Обрабатываю твою информацию...", reply_markup=ReplyKeyboardRemove())
     user = update.message.from_user
     folder = user.username
     if folder is None:
         folder = user.last_name
         if folder is None:
             folder = user.first_name
-    path = os.getcwd() + r"\{}".format(folder) + r"\{}".format(context.user_data['table'])
+    path = os.getcwd() + r"/{0}/{1}".format(folder,context.user_data['table'])
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
 
     cursor.execute(
-        f"""ALTER TABLE {context.user_data['table'][:-3]} ADD {context.user_data['colName']} {context.user_data['type']};""")
+        f"""ALTER TABLE {context.user_data['table'][:-3]} ADD {context.user_data['colName']} {context.user_data['type']};"""
+    )
     connection.commit()
     update.message.reply_text("Готово!")
-    context.bot.send_document(chat_id=update.effective_chat.id, document=open(path, 'rb'))
+    context.bot.send_document(
+        chat_id=update.effective_chat.id, document=open(path, 'rb'))
     return ConversationHandler.END
 
 
 def cancel(update, context):
     print("cancel")
-    update.message.reply_text("Бип Бип, прерываю диалог. Пиши /help для других команд")
+    update.message.reply_text(
+        "Бип Бип, прерываю диалог. Пиши /help для других команд")
     return ConversationHandler.END
 
 
 if __name__ == '__main__':
-    file = os.getcwd() + r"\users.db"
+    file = os.getcwd() + r"/users.db"
     create_connection(file)
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', _help))
@@ -256,35 +298,49 @@ if __name__ == '__main__':
         entry_points=[CommandHandler('create_db', startCreatingDB)],
         states={
             1: [MessageHandler(Filters.text, getDBName)],
-
             2: [MessageHandler(Filters.text, column, pass_user_data=True)],
-
             3: [MessageHandler(Filters.text, columnType, pass_user_data=True)],
-
-            4: [MessageHandler(Filters.text, createDBUserData, pass_user_data=True)]
+            4: [
+                MessageHandler(
+                    Filters.text, createDBUserData, pass_user_data=True)
+            ]
         },
         fallbacks=[])
 
     searchSQLCommandsConvHandler = ConversationHandler(
         entry_points=[CommandHandler('sql', askSQLCommand)],
         states={
-            1: [MessageHandler(Filters.text, sendLinkToSQLCommand, pass_user_data=True)]
+            1: [
+                MessageHandler(
+                    Filters.text, sendLinkToSQLCommand, pass_user_data=True)
+            ]
         },
         fallbacks=[])
     fillTableConvHandler = ConversationHandler(
         entry_points=[CommandHandler('insert', startFillingTable)],
         states={
-            1: [MessageHandler(Filters.text, getTableNameToFill, pass_user_data=True)],
-            2: [MessageHandler(Filters.text, getColumnNameToFill, pass_user_data=True)],
-            3: [MessageHandler(Filters.text, fillTableWithUserData, pass_user_data=True)]
+            1: [
+                MessageHandler(
+                    Filters.text, getTableNameToFill, pass_user_data=True)
+            ],
+            2: [
+                MessageHandler(
+                    Filters.text, getColumnNameToFill, pass_user_data=True)
+            ],
+            3: [
+                MessageHandler(
+                    Filters.text, fillTableWithUserData, pass_user_data=True)
+            ]
         },
         fallbacks=[])  # CommandHandler('cancel', cancel)
 
     alterTableConvHandler = ConversationHandler(
         entry_points=[CommandHandler('add_column', startFillingTable)],
         states={
-            1: [MessageHandler(Filters.text, newColumnName, pass_user_data=True)],
-            2: [MessageHandler(Filters.text, newColumnType, pass_user_data=True)],
+            1:
+            [MessageHandler(Filters.text, newColumnName, pass_user_data=True)],
+            2:
+            [MessageHandler(Filters.text, newColumnType, pass_user_data=True)],
             3: [MessageHandler(Filters.text, alterTable, pass_user_data=True)]
         },
         fallbacks=[])  # CommandHandler('cancel', cancel)
